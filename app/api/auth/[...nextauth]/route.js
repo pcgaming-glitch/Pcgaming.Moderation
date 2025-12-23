2ce4f40de597aff08c90320678031802bfcc1fb8
pcgaming-glitch <dauntaythomas@yahoo.com>
2025-12-23 01:23:56 +0100

    Create route.js

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+
+const handler = NextAuth({
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET
+    })
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      session.user.id = token.sub;
+      return session;
+    }
+  }
+});
+
+export { handler as GET, handler as POST };
